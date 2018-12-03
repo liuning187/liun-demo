fe1dc6d5cb6aff973b3b9f36ae51e7186e7d4f9f
liuning187 <44642399+liuning187@users.noreply.github.com>
2018-12-03 14:19:46 +0800

    Update LiuNingDemoApplication.java

--- src/main/java/com/primeton/liuning/demo/LiuNingDemoApplication.java
+++ src/main/java/com/primeton/liuning/demo/LiuNingDemoApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan({"com.primeton.liuning.demo.dao"})
 @EnableSwagger2
+@EnableTransactionManagement
 public class LiuNingDemoApplication {
 
     public static void main(String[] args) {
